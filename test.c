5a0a7201e14fd7ffa6706f949bd9b0f0a0bb9ba5
Fritz Lang <137324866+FritzLang1025@users.noreply.github.com>
2025-10-14 16:44:06 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,12 +1,11 @@
-#include "SqList.h"
+#include "LinkList.h"
 
 int main() {
-    SqList L;
-    InitLsit(&L);
-    ListInsert(&L, 1, 1);
-    ListInsert(&L, 2, 2);
-    ListInsert(&L, 3, 4);
-    ListInsert(&L, 4, 8);
-    ListInsert(&L, 5, 16);
-    printf("%d", LocateElem(L, 8));
+    LinkList L;
+    InitList(&L);
+    List_HeadInsert(&L, 5);
+    ListInsert(&L, 2, 1);
+    PrintList(L);
+
+    return 0;
 }
